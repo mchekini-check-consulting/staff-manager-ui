2d2271fe65301bd1fb6dc2c216220e64b0a287e9
samlogy <aceportgasd235@gmail.com>
2023-07-10 19:23:35 +0100

    add: calendar component + configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {TemplateComponent} from './core/template/container/template.component';
-import {FooterComponent} from './core/template/components/footer/footer.component';
-import {NavbarComponent} from './core/template/components/navbar/navbar.component';
-import {SidebarComponent} from './core/template/components/sidebar/sidebar.component';
-import {CraComponent} from './features/cra/cra.component';
-import {JustificatifsComponent} from './features/justificatifs/justificatifs.component';
-import {FicheDePaieComponent} from './features/fiche-de-paie/fiche-de-paie.component';
-import {DocumentationComponent} from './features/documentation/documentation.component';
-import {ProfileInformationComponent} from './features/profile-information/profile-information.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {OAuthModule} from "angular-oauth2-oidc";
-import {HttpRequestInterceptor} from "./core/interceptors/HttpRequestInterceptor";
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './core/template/container/template.component';
+import { FooterComponent } from './core/template/components/footer/footer.component';
+import { NavbarComponent } from './core/template/components/navbar/navbar.component';
+import { SidebarComponent } from './core/template/components/sidebar/sidebar.component';
+import { CraComponent } from './features/cra/cra.component';
+import { JustificatifsComponent } from './features/justificatifs/justificatifs.component';
+import { FicheDePaieComponent } from './features/fiche-de-paie/fiche-de-paie.component';
+import { DocumentationComponent } from './features/documentation/documentation.component';
+import { ProfileInformationComponent } from './features/profile-information/profile-information.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpRequestInterceptor } from './core/interceptors/HttpRequestInterceptor';
+import { FullCalendarModule } from '@fullcalendar/angular';
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,16 +33,16 @@ import {HttpRequestInterceptor} from "./core/interceptors/HttpRequestInterceptor
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    OAuthModule.forRoot()
+    FullCalendarModule,
+    OAuthModule.forRoot(),
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpRequestInterceptor,
       multi: true,
-    }
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/features/cra/cra.component.ts
+++ src/app/features/cra/cra.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
+import {
+  CalendarOptions,
+  DateSelectArg,
+  EventClickArg,
+  EventInput,
+} from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { Store } from '@ngrx/store';
+import * as CalendarActions from './actions';
+import { CalendarFeature, selectEventsCount } from './reducer';
+import { createEventId } from './event-utils';
 
 @Component({
   selector: 'app-cra',
   templateUrl: './cra.component.html',
-  styleUrls: ['./cra.component.scss']
+  styleUrls: ['./cra.component.scss'],
 })
 export class CraComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    // dateClick: this.handleDateClick.bind(this), // MUST ensure `this` context is maintained
+    events: [
+      { title: 'event 1', date: '2019-04-01' },
+      { title: 'event 2', date: '2019-04-02' },
+    ],
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
 
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+    },
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    // eventClick: this.handleEventClick.bind(this),
+  };
+
+  toggleWeekends() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends; // toggle the boolean!
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    // const title = prompt('Please enter a new title for your event');
+    // const calendarApi = selectInfo.view.calendar;
+    // calendarApi.unselect(); // clear date selection
+    // if (title) {
+    //   const event: EventInput = {
+    //     id: createEventId(),
+    //     title,
+    //     start: selectInfo.startStr,
+    //     end: selectInfo.endStr,
+    //     allDay: selectInfo.allDay,
+    //   };
+    //   // this.store.dispatch(CalendarActions.createEvent({ event }));
+    // }
+    console.log(selectInfo);
+    const calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect(); // clear selected date
+
+    // open modal
+    // fill form
+    // save form (api call)
+    // close pop-up
+  }
 }
