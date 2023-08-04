39d0788d7024ba84c24614a370b833dd16c6fddb
mahdi chekini <mchekini@checkconsulting.fr>
2023-08-04 02:16:46 +0200

    update keycloak url

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,11 +2,11 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'http://ci.check-consulting.net:10000/auth/realms/staff-manager-collab',
+  issuer: 'https://keycloak.check-consulting.net/auth/realms/staff-manager-collab',
   redirectUri: window.location.origin,
   clientId: 'staff-manager-client',
   responseType: 'code',
-  logoutUrl : 'http://ci.check-consulting.net:10000/auth/realms/staff-manager-collab/protocol/openid-connect/logout',
+  logoutUrl : 'https://keycloak.check-consulting.net/auth/realms/staff-manager-collab/protocol/openid-connect/logout',
   postLogoutRedirectUri: window.location.origin,
   showDebugInformation: true,
   requireHttps: false
