7e8ef0b2559b75520e8ed2f2c70db870e851740d
mahdi chekini <mchekini@checkconsulting.fr>
2023-10-05 00:50:11 +0200

    update jenkinsfile and keycloka configuration

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,12 +2,13 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'https://keycloak.check-consulting.net/auth/realms/staff-manager-collab',
+  issuer: 'http://ci.check-consulting.net:10000/auth/realms/staff-manager-collab',
   redirectUri: window.location.origin,
   clientId: 'staff-manager-client',
   responseType: 'code',
-  logoutUrl : 'https://keycloak.check-consulting.net/auth/realms/staff-manager-collab/protocol/openid-connect/logout',
+  logoutUrl : 'http://ci.check-consulting.net:10000/auth/realms/staff-manager-collab/protocol/openid-connect/logout',
   postLogoutRedirectUri: window.location.origin,
   showDebugInformation: true,
   requireHttps: false
 };
+
